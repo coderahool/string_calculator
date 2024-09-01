90868b26be4fb7be266e303df5e04c98256e31b4
Rahul Kewalramani <rahool.kewalramani@gmail.com>
2024-09-01 15:48:13 +0530

    Add Calculator class and implement add method to handle empty strings (WIP)

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -2,4 +2,9 @@ require 'spec_helper'
 require './lib/calculator'
 
 RSpec.describe  Calculator do
+  describe "#add" do
+    it "adds an empty string" do
+      expect(Calculator.new.add("")).to eq(0)
+    end
+  end
 end
