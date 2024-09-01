c5fddc3cce7564d11bf36e32c98f1ccd428ca233
Rahul Kewalramani <rahool.kewalramani@gmail.com>
2024-09-01 18:02:41 +0530

    Rubocop fixes

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,54 +1,51 @@
+# frozen_string_literal: true
 require 'spec_helper'
 require './lib/calculator'
 
-RSpec.describe  Calculator do
-  describe "#add" do
-    it "adds an empty string" do
-      expect(Calculator.new.add("")).to eq(0)
+RSpec.describe Calculator do
+  describe '#add' do
+    it 'adds an empty string' do
+      expect(Calculator.new.add('')).to eq(0)
     end
 
-    it "adds a single number" do
-      expect(Calculator.new.add("5")).to eq(5)
+    it 'adds a single number' do
+      expect(Calculator.new.add('5')).to eq(5)
     end
 
-    it "handles a large number of numbers" do
-      numbers = (1..1000).to_a.join(",")
-      expect(Calculator.new.add(numbers)).to eq(500500)
+    it 'handles a large number of numbers' do
+      numbers = (1..1000).to_a.join(',')
+      expect(Calculator.new.add(numbers)).to eq(500_500)
     end
 
-    it "handles newlines between numbers" do
+    it 'handles newlines between numbers' do
       expect(Calculator.new.add("1\n2,3")).to eq(6)
     end
 
-    it "handles custom delimiters" do
+    it 'handles custom delimiters' do
       expect(Calculator.new.add("//;\n1;2")).to eq(3)
     end
 
-    it "handles other custom delimiters" do
-      expect(Calculator.new.add("//|\n2|2")).to eq(4)
-    end
-
-    it "handles empty strings with delimiters" do
+    it 'handles empty strings with delimiters' do
       expect(Calculator.new.add("//;\n")).to eq(0)
     end
 
-    it "throws an exception for negative numbers" do
-      expect { Calculator.new.add("-1,-4,2") }.to raise_error(ArgumentError, "negative numbers not allowed -1, -4")
+    it 'throws an exception for negative numbers' do
+      expect { Calculator.new.add('-1,-4,2') }.to raise_error(ArgumentError, 'negative numbers not allowed -1, -4')
     end
 
-    it "ignores numbers greater than 1000" do
-      expect(Calculator.new.add("2,1001")).to eq(2)
+    it 'ignores numbers greater than 1000' do
+      expect(Calculator.new.add('2,1001')).to eq(2)
     end
 
-    it "handles delimiters of arbitrary length" do
+    it 'handles delimiters of arbitrary length' do
       expect(Calculator.new.add("//[***]\n1***2***3")).to eq(6)
     end
 
-    it "handles multiple custom delimiters" do
+    it 'handles multiple custom delimiters' do
       expect(Calculator.new.add("//[*][%]\n1*2%3")).to eq(6)
     end
 
-    it "handles multiple custom delimiters of any length" do
+    it 'handles multiple custom delimiters of any length' do
       expect(Calculator.new.add("//[***][%%]\n1***2%%3")).to eq(6)
     end
   end
